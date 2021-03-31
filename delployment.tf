17ddf468ddbba2beffd53266e7e58dc6988f615c
Vincent Gauthier <vincent.d.gauthier@protonmail.com>
2021-03-31 17:29:24 -0400

    Implemented dns role
    Via cloudflare's api

--- delployment.tf
+++ delployment.tf
@@ -12,6 +12,10 @@ terraform {
       source = "linode/linode"
       version = "1.14.3"
     }
+    # namecheap = {
+    #   source  = "robgmills/namecheap"
+    #   version = "1.7.0"
+    # }
   }
 }
 
@@ -22,7 +26,7 @@ provider "linode" {
 
 resource "linode_instance" "web_blog" {
   label = "web_blog"
-  image = "linode/centos8"
+  image = "linode/ubuntu20.04"
   region = "us-east"
   type = "g6-nanode-1"
 
@@ -42,7 +46,29 @@ resource "linode_instance" "web_blog" {
     }
   }
 
+
+  provisioner "local-exec"{
+    command = "sed -i '/remote_ip/d' ./roles/dns_setup/vars/variables.yml && echo -e '\nremote_ip: ${self.ip_address}' >> ./roles/dns_setup/vars/variables.yml"
+  }
+
   provisioner "local-exec"{
     command = "ssh-keyscan ${self.ip_address} >> ~/.ssh/known_hosts && echo -e '[blog]\n${self.ip_address}' > inventory && ansible-playbook -i inventory --private-key='${var.private_key_path}' --extra-vars 'ansible_user=root' provision.yml"
   }
 }
+
+# provider "namecheap" {
+#   username = "your_username" # Also set by env variable `NAMECHEAP_USERNAME`
+#   api_user = "your_username" # Same as username; also set by env variable `NAMECHEAP_API_USER`
+#   token = "your_token" # Also set by env variable `NAMECHEAP_TOKEN`
+#   ip = "your.ip.address.here" # Also set by env variable `NAMECHEAP_IP`
+#   use_sandbox = false # Toggle for testing/sandbox mode; Also set by env variable `NAMECHEAP_USE_SANDBOX`
+# }
+
+# # Create a DNS A Record for a domain you own
+# resource "namecheap_record" "www-example-com" {
+#   name = "www"
+#   domain = "example.com"
+#   address = "127.0.0.1"
+#   mx_pref = 10
+#   type = "A"
+# }
